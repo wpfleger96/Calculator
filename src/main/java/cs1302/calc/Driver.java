a1b75fcd4bb1fb5e8da898ad766148738d7311cf
Michael E. Cotterell <mepcotterell@gmail.com>
2012-09-30 19:31:58 -0400

    updated with initial files

---
+++ src/main/java/cs1302/calc/Driver.java
@@ -0,0 +1,17 @@
+package cs1302.calc;
+
+/**
+ * This class is the entry point into the application.
+ */
+public class Driver {
+
+    /**
+     * The main entry point into the application.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+    } // main
+
+} // Driver
