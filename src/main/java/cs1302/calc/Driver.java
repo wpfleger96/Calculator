adce88559b7ba3ad24adb882f4b365988eaaf65a
Michael E. Cotterell <mepcotterell@gmail.com>
2012-09-30 19:56:22 -0400

    updated the skeleton code

--- src/main/java/cs1302/calc/Driver.java
+++ src/main/java/cs1302/calc/Driver.java
@@ -11,7 +11,11 @@ public class Driver {
      * @param args command line arguments
      */
     public static void main(String[] args) {
-
+	javax.swing.SwingUtilities.invokeLater(new Runnable() {
+	    public void run() {
+                Calculator.createAndShowGUI();
+	    } // run
+	});
     } // main
 
 } // Driver
