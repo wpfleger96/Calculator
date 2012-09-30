61548a65cfacd3c8f28e1748da46fbabb97fed05
Michael E. Cotterell <mepcotterell@gmail.com>
2012-09-30 19:39:35 -0400

    added dummy test for Driver

---
+++ src/test/java/cs1302/calc/DriverTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This is a test for the Driver class. Doesn't actually do anything/
+ *
+ * @author Michael E. Cotterell <mepcotterell@gmail.com>
+ */
+public class DriverTest {
+
+  @Test
+  public void testMain() {
+      assertEquals("Driver Test", true, true);
+  } // testMain
+
+} // DriverTest
+
