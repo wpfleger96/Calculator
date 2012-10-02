e28d73bb2ec92d8340732c8844d4715295f51e06
Michael E. Cotterell <mepcotterell@gmail.com>
2012-10-01 23:36:47 -0400

    Updated the evaluate method in ReversePolishNotation

--- src/main/java/cs1302/calc/Math.java
+++ src/main/java/cs1302/calc/Math.java
@@ -1,6 +1,68 @@
 package cs1302.calc;
 
-public class Math {
+/** 
+ * Provides an interface for different mathematical operations
+ */
+public interface Math {
+
+    /**
+     * Returns the result of the binary addition operation "lhs + rhs"
+     *
+     * @param lhs the first operand
+     * @param rhs the second operand
+     * @return the result of the operation
+     */
+    public double add(double lhs, double rhs);
+
+    /**
+     * Returns the result of the binary subtraction operation "lhs - rhs"
+     *
+     * @param lhs the first operand
+     * @param rhs the second operand
+     * @return the result of the operation
+     */
+    public double subtract(double lhs, double rhs);
+
+    /**
+     * Returns the result of the binary multiplication operation "lhs * rhs"
+     *
+     * @param lhs the first operand
+     * @param rhs the second operand
+     * @return the result of the operation
+     */
+    public double multiply(double lhs, double rhs);
+
+    /**
+     * Returns the result of the binary division operation "lhs / rhs". This
+     * method should throw an ArithmeticException if the second operand is equal
+     * to zero.
+     *
+     * @param lhs the first operand
+     * @param rhs the second operand
+     * @return the result of the operation
+     */
+    public double divide(double lhs, double rhs) throws ArithmeticException;
+
+    /**
+     * Returns the result of the unary factorial operation "num !". This method
+     * should throw a DomainException if the operand cannot be converted into an
+     * integer.
+     *
+     * @param num the operand
+     * @return the result of the operation
+     */
+    public double factorial(double num) throws DomainException;
+
+    /**
+     * Returns the result of the binary exponentiation operation "lhs ^ rhs". 
+     * This method should throw a DomainException if the second operand cannot
+     * be converted into an integer.
+     *
+     * @param lhs the first operand
+     * @param rhs the second operand
+     * @return the result of the operation
+     */
+    public double pow(double lhs, double rhs) throws DomainException;
 
 } // Math
 
--- src/main/java/cs1302/calc/ReversePolishNotation.java
+++ src/main/java/cs1302/calc/ReversePolishNotation.java
@@ -7,8 +7,22 @@ import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
+/**
+ * Contains static methods for working with the Reverse Polish Notation of
+ * certain mathematical expressions.
+ *
+ * The following binary operations are supported: addition (+), 
+ * subtraction (-), multiplication (*), division(/), and exponentiation (^).
+ * Note that ^ is used here for exponentiation and not bitwise exclusive OR (as 
+ * is the case for ^ in Java)
+ *
+ * The following unary operations are supported: factorial (!).
+ *
+ * @author Michael E. Cotterell <mepcotterell@gmail.com>
+ */
 public class ReversePolishNotation {
 
+    // map for storing the precedence levels of each operator
     private static final Map<String, Integer> precedenceMap;
 
     static {
@@ -18,13 +32,36 @@ public class ReversePolishNotation {
         pMap.put("*", 2);
         pMap.put("/", 2);
 	pMap.put("!", 3);
+	pMap.put("^", 4);
 	precedenceMap = Collections.unmodifiableMap(pMap);
     } // static
 
+    /** 
+     * Converts an expression expressed in infix notation as an array of Strings
+     * to the appropriate expression expressed in postfix notationan as an array 
+     * of Strings.
+     *
+     * <p>
+     * Here is an example of an expression in infix notation:
+     * <code>4 ! + 2 / 3 - 7 * 2 ^ 3</code>
+     * Each element of this expression would be an element in the array that is
+     * passed into this method.
+     *
+     * <p>
+     * The resulting postfix expression is:
+     * <code>4 ! 2 3 / + 7 2 3 ^ * -</code>
+     * Each element of this expression would be an element in the array that is
+     * returned by this method.
+     *
+     * @param infix an array containing an infix expression
+     * @return an array containing the postfix expression
+     */
     public static String[] infixToPostfix(String[] infix) {
 
+	// a list for the resulting postfix expression
 	List<String> postfix = new ArrayList<String>();
 
+	// a stack for implementing the conversion
 	Stack<String> operatorStack = new Stack<String>();
 
 	// check the length of the expression
@@ -67,5 +104,73 @@ public class ReversePolishNotation {
         return postfix.toArray(new String[postfix.size()]);
 
     } // infix2postfix
+
+    /**
+     * Evaluates a mathematical expression expressed in postfix notation. This 
+     * method may return a DomainException if one of the operands for an
+     * operation is not in correct number set. It may also return a NumberFormat
+     *
+     * @param impl an instance of a Math implementation
+     * @param postfix the mathematical expression in postfix notation
+     * @return the result of evaluating the expression
+     */
+    public static double evaluate(Math impl, String[] postfix) throws ArithmeticException, DomainException, NumberFormatException {
+
+	// a stack for implementing the evaluation
+	Stack<Double> stack = new Stack<Double>();
+
+	for (int i = 0; i < postfix.length; i++) {
+
+	    if (!Character.isDigit(postfix[i].charAt(0))) {
+
+		// if the first character of the element is not a digit then we 
+		// assume it is an operator
+
+		String operator = postfix[i];
+		
+		if (operator.equals("+")) {
+		    double rhs = stack.pop();
+		    double lhs = stack.pop();
+		    double result = impl.add(lhs, rhs);
+		    stack.add(result);
+		} else if (operator.equals("-")) {
+		    double rhs = stack.pop();
+		    double lhs = stack.pop();
+		    double result = impl.subtract(lhs, rhs);
+		    stack.add(result);
+		} else if (operator.equals("*")) {
+		    double rhs = stack.pop();
+		    double lhs = stack.pop();
+		    double result = impl.multiply(lhs, rhs);
+		    stack.add(result);
+		} else if (operator.equals("/")) {
+		    double rhs = stack.pop();
+		    double lhs = stack.pop();
+		    double result = impl.divide(lhs, rhs);
+		    stack.add(result);
+		} else if (operator.equals("^")) {
+		    double rhs = stack.pop();
+		    double lhs = stack.pop();
+		    double result = impl.pow(lhs, rhs);
+		    stack.add(result);
+		} else if (operator.equals("!")) {
+		    double num = stack.pop();
+		    double result = impl.factorial(num);
+		    stack.add(result);
+		} // if
+
+	    } else {
+
+		// otherwise we assume it is an operand and add it to the stack
+		stack.add(Double.parseDouble(postfix[i]));
+
+	    } // if
+
+	} // for
+
+	// the only element left in the stack will be the result of the evaluation
+	return stack.pop();
+
+    } // evaluate
 
 } // ReversePolishNotation
