24a9965766fd431e9f97ac1d3ea48a56bf247ea3
Michael E. Cotterell <mepcotterell@gmail.com>
2012-10-01 23:43:16 -0400

    cleaned up some comments

--- src/main/java/cs1302/calc/DomainException.java
+++ src/main/java/cs1302/calc/DomainException.java
@@ -1,5 +1,9 @@
 package cs1302.calc;
 
+/**
+ * Represents a domain mismatch in an involving an operand in a mathematical
+ * operation.
+ */
 public class DomainException extends Exception {
     
     public DomainException(String message) {
