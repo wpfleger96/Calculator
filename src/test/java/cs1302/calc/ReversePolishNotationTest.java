99a579ef7e15d760df6ef036fc44a00b29f602fa
Michael E. Cotterell <mepcotterell@gmail.com>
2013-04-14 18:34:57 -0400

    Updated the unit test for ReversePolishNotation

--- src/test/java/cs1302/calc/ReversePolishNotationTest.java
+++ src/test/java/cs1302/calc/ReversePolishNotationTest.java
@@ -28,7 +28,7 @@ public class ReversePolishNotationTest {
 	{"2 ^ 3", "2 3 ^", "8"},
 	{"222 + 32 * 5 + 1", "222 32 5 * + 1 +", "383"},
 	{"4 !", "4 !", "24"},
-	{"4 - 2 ! / 3 + 7 * 2 ^ 3", "4 2 ! 3 / - 7 2 3 ^ * +", "59"},
+	{"4 - 2 ! / 3 + 7 * 2 ^ 3", "4 2 ! 3 / - 7 2 3 ^ * +", "60"},
 	{"45 / 10", "45 10 /", "4"},
 	{"9 < 2", "9 2 <", "36"},
 	{"2 * 3 ! ^ 5 + 9", "2 3 ! 5 ^ * 9 +", "15561"}
@@ -54,6 +54,32 @@ public class ReversePolishNotationTest {
 	} // for
 
     } // testInfixToPosrfix
+
+    @Test
+    public void testEvaluate() {
+
+	for (String[] example: examples) {
+
+	    String   expression = example[0];
+	    String[] infix      = expression.split(" ");
+	    String[] postfix    = ReversePolishNotation.infixToPostfix(infix);
+	    int      expected   = -2;
+	    int      result     = -1;
+
+	    try {
+		expected = Integer.parseInt(example[2]);
+	    } catch (Exception e) { }
+
+	    try {
+		result = ReversePolishNotation.evaluate(impl, postfix);
+	    } catch (Exception e) { }
+
+	    String message = String.format("\"%s\" --> %d (expected %d)", expression, result, expected);
+	    assertTrue(message, result == expected);
+
+	} // for	
+
+    } // testEvaluate
 
 } // ReversePolishNotationTest
 
