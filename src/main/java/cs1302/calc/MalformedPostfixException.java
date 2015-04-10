2c3bfe845a2e50824fcf3e7148a9d45fb64a01c6
Michael E. Cotterell <mepcotterell@gmail.com>
2015-04-10 09:17:58 -0400

    updated the exception so that it's unchecked

--- src/main/java/cs1302/calc/MalformedPostfixException.java
+++ src/main/java/cs1302/calc/MalformedPostfixException.java
@@ -3,12 +3,12 @@ package cs1302.calc;
 import java.util.Arrays;
 
 /**
- * Checked exception thrown when attempting to evaluate a malformed postfix
+ * An unchecked exception thrown when attempting to evaluate a malformed postfix
  * expression.
  *
  * @author Michael E. Cotterell
  */
-public class MalformedPostfixException extends Exception {
+public class MalformedPostfixException extends ArithmeticException {
 
     public MalformedPostfixException(String[] postfix) {
         super(String.format("malformed: %s", Arrays.toString(postfix)));
