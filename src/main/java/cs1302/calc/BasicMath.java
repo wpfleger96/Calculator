1adde7bfc09b865fa2d78b009c8e4f206522734b
Michael E. Cotterell <mepcotterell@gmail.com>
2015-04-19 19:35:53 -0400

    updated basic math

--- src/main/java/cs1302/calc/BasicMath.java
+++ src/main/java/cs1302/calc/BasicMath.java
@@ -5,54 +5,45 @@ package cs1302.calc;
  * interface. This implementation assumes that the inputs to each method are
  * non-negative integers.
  *
- * @author Michael E. Cotterell <mepcotterell@gmail.com>
+ * @author Michael E. Cotterell 
  */
 public class BasicMath implements Math {
 
-    public int inc(int n) {
-	return n + 1;
-    } // inc
-
-    public int dec(int n) {
-        if (n == 0) return 0;
-	return n - 1;
-    } // dec
-
     public int add(int lhs, int rhs) {
-	return lhs + rhs;
+        return lhs + rhs;
     } // add
 
     public int sub(int lhs, int rhs) {
-	if (lhs - rhs < 0) return 0;
+        if (lhs - rhs < 0) return 0;
         return lhs - rhs;
     } // sub
 
     public int mul(int lhs, int rhs) {
-	return lhs * rhs;
+        return lhs * rhs;
     } // mul
 
     public int div(int lhs, int rhs) throws ArithmeticException {
-	return lhs / rhs;
+        return lhs / rhs;
     } // div
 
     public int fac(int num) {
-	// THE NARWHAL BACONS!
-	int product = 1;              // products start with one
-	int n       = ++num;          // increment, like a boss
+        // THE NARWHAL BACONS!
+        int product = 1;              // products start with one
+        int n       = ++num;          // increment, like a boss
         while (n --> 2) product *= n; // while n approaches 2, multiply
-	return product;               // wait, "-->",  WAT!?
+        return product;               // wait, "-->",  WAT!?
     } // fac
 
     public int pow(int lhs, int rhs) {
-	return (int) java.lang.Math.pow(lhs, rhs);
+        return (int) java.lang.Math.pow(lhs, rhs);
     } // pow
 
     public int lshift(int lhs, int rhs) {
-	return lhs << rhs;
+        return lhs << rhs;
     } // lshift
 
     public int rshift(int lhs, int rhs) {
-	return lhs >> rhs;
+        return lhs >> rhs;
     } // rshift
 
 } // BasicMath
