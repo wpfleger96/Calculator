0d44c04aac660750b0116c7b69dc80dc7eb5b8c1
TungDo <johnnydo@uga.edu>
2015-11-15 17:43:38 -0500

    5:43

--- src/main/java/cs1302/calc/Driver.java
+++ src/main/java/cs1302/calc/Driver.java
@@ -56,15 +56,57 @@ public class Driver extends Application implements ActionListener {
 	VBox background = new VBox();
 	Label line1 = new Label("0000 0000 0000 0000 0000 0000 0000");
 	Label line2 = new Label("0000 0000 0000 0000 0000 0000 0000");
-	VBox buttons = new VBox();
+	VBox buttons = createButtons();
 	background.setSpacing(10);
 	background.getChildren().addAll(line1, line2, buttons);
 	return background;
 
     }
- 
+
+    public VBox createButtons(){
+
+	VBox buttons = new VBox();
+	HBox row1 = new HBox();
+	HBox row2 = new HBox();
+	HBox row3 = new HBox();
+	HBox row4 = new HBox();
+
+	Button n7 = new Button("7");
+	Button n8 = new Button("8");
+	Button n9 = new Button("9");
+	Button slash = new Button("/");
+	Button exclamation = new Button("!");
+	Button backspace  = new Button("<");
+	Button clear  = new Button("X");
+	Button n4 = new Button("4");
+	Button n5 = new Button("5");
+	Button n6 = new Button("6");
+	Button multiply = new Button("*");
+	Button power = new Button("^");
+	Button lshift = new Button("<<");
+	Button rshift = new Button(">>");
+	Button n1 = new Button("1");
+	Button n2 = new Button("2");
+	Button n3 = new Button("3");
+	Button subtract = new Button("-");
+	Button useRecurs = new Button("Use Recursion");
+	Button n0 = new Button("0");
+	Button equals = new Button("=");
+	Button add = new Button("+");
+	Button hideBin = new Button("Hide Binary");
+
+	row1.getChildren().addAll(n7, n8, n9, slash, exclamation, backspace, clear);
+	row2.getChildren().addAll(n4, n5, n6, multiply, power, lshift, rshift);
+	row3.getChildren().addAll(n1, n2, n3, subtract, useRecurs);
+	row4.getChildren().addAll(n0, equals, add, hideBin);
+	buttons.getChildren().addAll(row1, row2, row3, row4);
+
+	return buttons;
+    }
+
 
     public void actionPerformed(ActionEvent e){
+	
     }
 
 } // Driver
