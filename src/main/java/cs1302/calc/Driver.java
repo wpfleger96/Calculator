2ee1459071412f4939a0dfcd8b169518321cb6f4
William <willpfleger@yahoo.com>
2015-11-15 18:57:00 -0500

    equals button updated

--- src/main/java/cs1302/calc/Driver.java
+++ src/main/java/cs1302/calc/Driver.java
@@ -18,6 +18,7 @@ import javafx.scene.layout.*;
 public class Driver extends Application{
 
     private Label argument, answer;
+    private boolean useRecursion=false;
 
     public static void main(String[] args) {
         launch(args);
@@ -76,60 +77,81 @@ public class Driver extends Application{
 	Button n7 = new Button("7");
 	n7.setOnAction(e -> argument.setText(argument.getText() + " 7"));
 	Button n8 = new Button("8");
-	n8.setonAction(e -> argument.setText(argument.getText() + " 8"));
+	n8.setOnAction(e -> argument.setText(argument.getText() + " 8"));
 	Button n9 = new Button("9");
-	n9.setonAction(e -> argument.setText(argument.getText() + " 9"));
+	n9.setOnAction(e -> argument.setText(argument.getText() + " 9"));
 	Button slash = new Button("/");
-	slash.setonAction(e -> argument.setText(argument.getText() + " /"));
+	slash.setOnAction(e -> argument.setText(argument.getText() + " /"));
 	Button exclamation = new Button("!");
-	exclamation.setonAction(e -> argument.setText(argument.getText() + " !"));
+	exclamation.setOnAction(e -> argument.setText(argument.getText() + " !"));
 	Button backspace  = new Button("<");
-	backspace.setonAction(e -> argument.setText(argument.getText().substring(0,argument.getText().length()-2));
+	backspace.setOnAction(e -> argument.setText(argument.getText().substring(0,argument.getText().length()-2)));
 	Button clear  = new Button("X");
-	clear.setonAction(e -> argument.setText(argument.setText(""));
+        clear.setOnAction(e -> argument.setText(""));
 	// Row 2
 	Button n4 = new Button("4");
-	n4.setonAction(e -> argument.setText(argument.getText() + " 4"));
+	n4.setOnAction(e -> argument.setText(argument.getText() + " 4"));
 	Button n5 = new Button("5");
-	n5.setonAction(e -> argument.setText(argument.getText() + " 5"));
+	n5.setOnAction(e -> argument.setText(argument.getText() + " 5"));
 	Button n6 = new Button("6");
-	n6.setonAction(e -> argument.setText(argument.getText() + " 6"));
+	n6.setOnAction(e -> argument.setText(argument.getText() + " 6"));
 	Button multiply = new Button("*");
-	multiply.setonAction(e -> argument.setText(argument.getText() + " *"));
+	multiply.setOnAction(e -> argument.setText(argument.getText() + " *"));
 	Button power = new Button("^");
-	power.setonAction(e -> argument.setText(argument.getText() + " ^"));
+	power.setOnAction(e -> argument.setText(argument.getText() + " ^"));
 	Button lshift = new Button("<<");
-	lshift.setonAction(e -> argument.setText(argument.getText() + " <<"));
+	lshift.setOnAction(e -> argument.setText(argument.getText() + " <<"));
 	Button rshift = new Button(">>");
-	rshift.setonAction(e -> argument.setText(argument.getText() + " >>"));
+	rshift.setOnAction(e -> argument.setText(argument.getText() + " >>"));
 	// Row 3
 	Button n1 = new Button("1");
-       	n1.setonAction(e -> argument.setText(argument.getText() + " 1"));
+       	n1.setOnAction(e -> argument.setText(argument.getText() + " 1"));
 	Button n2 = new Button("2");
-	n2.setonAction(e -> argument.setText(argument.getText() + " 2"));
+	n2.setOnAction(e -> argument.setText(argument.getText() + " 2"));
 	Button n3 = new Button("3");
-	n3.setonAction(e -> argument.setText(argument.getText() + " 3"));
+	n3.setOnAction(e -> argument.setText(argument.getText() + " 3"));
 	Button subtract = new Button("-");
-	subtract.setonAction(e -> argument.setText(argument.getText() + " -"));
-	Button useRecurs = new Button("Use Recursion");
+	subtract.setOnAction(e -> argument.setText(argument.getText() + " -"));
+	Button toggleMath = new Button("Use Recursion");
+        toggleMath.setOnAction(e -> toggleMathAlgorithm());
 
 	// Row 4
 	Button n0 = new Button("0");
-	n0.setonAction(e -> argument.setText(argument.getText() + " 0"));
+	n0.setOnAction(e -> argument.setText(argument.getText() + " 0"));
 	Button equals = new Button("=");
-	equals.setonAction(e -> argument.setText(argument.getText() + ""));
+	equals.setOnAction(e -> answer.setText(evaluate(argument.getText())));
 	Button add = new Button("+");
-	add.setonAction(e -> argument.setText(argument.getText() + " +"));
+	add.setOnAction(e -> argument.setText(argument.getText() + " +"));
 	Button hideBin = new Button("Hide Binary");
      
 
 	row1.getChildren().addAll(n7, n8, n9, slash, exclamation, backspace, clear);
 	row2.getChildren().addAll(n4, n5, n6, multiply, power, lshift, rshift);
-	row3.getChildren().addAll(n1, n2, n3, subtract, useRecurs);
+	row3.getChildren().addAll(n1, n2, n3, subtract, toggleMath);
 	row4.getChildren().addAll(n0, equals, add, hideBin);
 	buttons.getChildren().addAll(row1, row2, row3, row4);
 
 	return buttons;
+    }
+			      
+    public void toggleMathAlgorithm(){
+    }
+ 
+    public String evaluate(String s){
+	String expression = s;
+	String infix[] = expression.split(" ");
+	String postfix[] = ReversePolishNotation.infixToPostfix(infix);
+	Math math;
+	if(useRecursion){
+	    math = new RecursiveMath();
+	    int result = ReversePolishNotation.evaluate(math, postfix);
+	    return Integer.toString(result);
+	}
+	else{
+	    math = new IterativeMath();
+	    int result = ReversePolishNotation.evaluate(math, postfix);
+	    return Integer.toString(result);
+	}
     }
 
 
